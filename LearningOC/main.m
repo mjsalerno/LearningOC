2aedb0307ef07b1ddc2ffdf2b3b98d531a37c179
John Smith <johnsmith@johns-air.ca.cewit.int>
2013-12-18 11:19:53 -0500

    Initial Commit

---
+++ LearningOC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LearningOC
+//
+//  Created by John Smith on 12/18/13.
+//  Copyright (c) 2013 John Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
