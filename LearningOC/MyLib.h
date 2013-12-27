c316d048d70645e38b48dab533cfbf436a8829b5
Michael Salerno <michael.j.salerno@stonybrook.edu>
2013-12-27 13:57:51 -0500

    added files that i will write my test lib with

---
+++ LearningOC/MyLib.h
@@ -0,0 +1,13 @@
+//
+//  MyLib.h
+//  LearningOC
+//
+//  Created by Mike Salerno on 12/27/13.
+//  Copyright (c) 2013 John Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyLib : NSObject
+
+@end
---
+++ LearningOC/MyLib.m
@@ -0,0 +1,13 @@
+//
+//  MyLib.m
+//  LearningOC
+//
+//  Created by Mike Salerno on 12/27/13.
+//  Copyright (c) 2013 John Smith. All rights reserved.
+//
+
+#import "MyLib.h"
+
+@implementation MyLib
+
+@end
