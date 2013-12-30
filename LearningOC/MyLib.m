9800051336a676d2dc49589ed47c4c764c0168c0
Michael Salerno <michael.j.salerno@stonybrook.edu>
2013-12-30 16:37:31 -0500

    fixed errors

--- LearningOC/MyLib.m
+++ LearningOC/MyLib.m
@@ -11,13 +11,16 @@
 
 @implementation MyLib
 
-nt generic_obj_c_intercept(NSString* class_name, NSString *concrete_class_name, NSString *method_name, NSObject* obj, int argc, __strong NSObject* argv[], uint32_t *returncode, uint32_t *reasoncode) {
+int generic_obj_c_intercept(NSString* class_name, NSString *concrete_class_name, NSString *method_name, NSObject* obj, int argc, __strong NSObject* argv[], uint32_t *returncode, uint32_t *reasoncode) {
     NSLog(@"objective c intercept");
+    
+    return 0;
 }
 
 int generic_obj_c_exit(NSString* class_name, NSString *concrete_class_name, NSString *method_name, NSObject* obj, int argc, __strong NSObject* argv[], uint32_t *returncode, uint32_t *reasoncode) {
     NSLog(@"objective c exit");
     
+    return 0;
 }
 
 @end
