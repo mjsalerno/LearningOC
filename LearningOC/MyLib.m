4b4de560d267c91f4404c865a8855b829c0b9876
Michael Salerno <michael.j.salerno@stonybrook.edu>
2013-12-30 16:36:31 -0500

    added an implementation to the things greg sent

--- LearningOC/MyLib.m
+++ LearningOC/MyLib.m
@@ -11,5 +11,13 @@
 
 @implementation MyLib
 
+nt generic_obj_c_intercept(NSString* class_name, NSString *concrete_class_name, NSString *method_name, NSObject* obj, int argc, __strong NSObject* argv[], uint32_t *returncode, uint32_t *reasoncode) {
+    NSLog(@"objective c intercept");
+}
+
+int generic_obj_c_exit(NSString* class_name, NSString *concrete_class_name, NSString *method_name, NSObject* obj, int argc, __strong NSObject* argv[], uint32_t *returncode, uint32_t *reasoncode) {
+    NSLog(@"objective c exit");
+    
+}
 
 @end
